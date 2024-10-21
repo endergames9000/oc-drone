bbd30812e224cb5e64bd09e866ef5f337acb74dd
endergames9000 <93162095+endergames9000@users.noreply.github.com>
2024-10-20 20:48:49 -0400

    Update drone.lua

--- drone.lua
+++ drone.lua
@@ -13,7 +13,7 @@ function part(name)
   return component.proxy(component.list(name)())
 end
 
-local repo = "https://raw.githubusercontent.com/osmarks/oc-drone/master/"
+local repo = "https://github.com/endergames9000/oc-drone/blob/master/"
 chat = part "chat"
 radar = part "radar"
 drone = part "drone"
